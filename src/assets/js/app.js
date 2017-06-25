2ff555c59cc403f63c342d84f98ebe33bbfeab76
Jan Klán <jan@beatee.org>
2017-06-25 16:04:56 +1000

    Columns prototype

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+$(document).ready(function() {
+    $('section').click(function(){
+        $('section').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    console.log($('section.active').css('width'));
+});
+
