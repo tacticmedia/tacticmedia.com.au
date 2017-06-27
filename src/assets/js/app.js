a9fc626a53a2aca47d2724eaa0052e835f5402f3
Jan Klán <jan@beatee.org>
2017-06-27 15:25:19 +1000

    Columns layout selected to be the one we use

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,1 @@
 $(document).foundation();
-
-$(document).ready(function() {
-    $('section').click(function(){
-        $('section').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    console.log($('section.active').css('width'));
-});
-
