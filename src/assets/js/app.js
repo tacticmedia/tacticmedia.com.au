58b5b7543bb9e9eb8d838dacf70c9dc03a6abf45
Jan Klán <jan@beatee.org>
2017-06-27 17:32:35 +1000

    Working on actual content

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,50 @@
 $(document).foundation();
+
+var arrowsEnabled = true;
+
+var pages = [
+    'index',
+    'photography',
+    'aerial-photography',
+    'redifi',
+    'software-development',
+    'contact'
+];
+
+$(document).keydown(function(e) {
+
+    let gotoPage = null;
+
+    if (e.which === 37 || e.which === 39) {
+
+
+        const url = window.location.pathname;
+        const currentPage = url.substring(url.lastIndexOf('/')+1).replace(/\.[^/.]+$/, "") || 'index';
+        const currentPageIndex = pages.findIndex(page => page === currentPage);
+
+        switch(e.which) {
+            case 37: // left
+
+                if (currentPageIndex === 0) {
+                    gotoPage = pages[pages.length-1];
+                } else {
+                    gotoPage = pages[currentPageIndex - 1];
+                }
+
+                break;
+            case 39: // right
+
+                if (currentPageIndex === pages.length - 1) {
+                    gotoPage = pages[0];
+                } else {
+                    gotoPage = pages[currentPageIndex + 1];
+                }
+
+                break;
+        }
+
+        if (pages.find(page => page === gotoPage)) {
+            window.location.href = gotoPage + '.html';
+        }
+    }
+});
