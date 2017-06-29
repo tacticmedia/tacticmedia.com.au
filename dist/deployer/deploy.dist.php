cfcdc17eedb0765a574977fc932a789f3a0dd9a7
Jan Klán <jan@beatee.org>
2017-06-29 17:44:14 +1000

    Added dist folder again

---
+++ dist/deployer/deploy.dist.php
@@ -0,0 +1,12 @@
+<?php
+
+die('unauthorized');
+
+$shfile = realpath(dirname(__FILE__).'/../../deploy.sh');
+
+if (file_exists($shfile)) {
+    exec('/usr/bin/nohup '.dirname(__FILE__).'/../../deploy.sh > /dev/null 2>&1 &');
+    echo 'done';
+} else {
+    echo 'file not found.';
+}
