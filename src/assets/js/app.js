fb70d229f1327a3ef593d086eb0019d42c77870a
Jan Klan <jan@klan.com.au>
2020-02-19 08:56:26 +1030

    Thrown away the unused javascript

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,12 +6,12 @@ import 'what-input';
 // it would execute earlier than we have assigned the global variable.
 // This is why we have to use CommonJS require() here since it doesn't
 // have the hoisting behavior.
-window.jQuery = $;
-require('foundation-sites');
+// window.jQuery = $;
+// require('foundation-sites');
 
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
+import './lib/foundation-explicit-pieces';
 
 
 $(document).foundation();
