232e4dae9a0da95c02f3d7ecfa04aa4d0051846e
Jan Klan <jan@klan.com.au>
2020-09-17 06:47:43 +0930

    Dropped the Foundation dependency

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,8 +12,6 @@ import fs            from 'fs';
 import webpackStream from 'webpack-stream';
 import webpack2      from 'webpack';
 import named         from 'vinyl-named';
-import uncss         from 'uncss';
-import autoprefixer  from 'autoprefixer';
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
@@ -22,7 +20,7 @@ const $ = plugins();
 const PRODUCTION = !!(yargs.argv.production);
 
 // Load settings from settings.yml
-const { PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
+const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
 
 function loadConfig() {
   let ymlFile = fs.readFileSync('config.yml', 'utf8');
@@ -30,9 +28,8 @@ function loadConfig() {
 }
 
 // Build the "dist" folder by running all of the below tasks
-// Sass must be run later so UnCSS can search for used classes in the others assets.
 gulp.task('build',
- gulp.series(clean, gulp.parallel(pages, javascript, images, copy, rootfiles), sass, vcardSass, styleGuide));
+ gulp.series(clean, gulp.parallel(pages, sass, javascript, images, copy, rootfiles)));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
@@ -70,14 +67,6 @@ function resetPages(done) {
   done();
 }
 
-// Generate a style guide from the Markdown content and HTML template in styleguide/
-function styleGuide(done) {
-  sherpa('src/styleguide/index.md', {
-    output: PATHS.dist + '/styleguide.html',
-    template: 'src/styleguide/template.html'
-  }, done);
-}
-
 function rootfiles() {
     return gulp.src(PATHS.rootfiles)
       .pipe(gulp.dest(PATHS.dist));
@@ -86,47 +75,17 @@ function rootfiles() {
 // Compile Sass into CSS
 // In production, the CSS is compressed
 function sass() {
-
-  const postCssPlugins = [
-    // Autoprefixer
-    autoprefixer(),
-
-    // UnCSS - Uncomment to remove unused styles in production
-    // PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
-  ].filter(Boolean);
-
-  return gulp.src('src/assets/scss/app.scss')
+  return gulp.src('src/assets/scss/*.scss')
     .pipe($.sourcemaps.init())
     .pipe($.sass({
       includePaths: PATHS.sass
     })
       .on('error', $.sass.logError))
-    .pipe($.postcss(postCssPlugins))
-    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + '/assets/css'))
-    .pipe(browser.reload({ stream: true }));
-}
-
-// Compile Sass into CSS
-// In production, the CSS is compressed
-function vcardSass() {
-
-  const postCssPlugins = [
-    // Autoprefixer
-    autoprefixer(),
-
-    // UnCSS - Uncomment to remove unused styles in production
-    // PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
-  ].filter(Boolean);
-
-  return gulp.src('src/assets/scss/vcard.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      includePaths: PATHS.sass
-    })
-      .on('error', $.sass.logError))
-    .pipe($.postcss(postCssPlugins))
+    .pipe($.autoprefixer({
+      browsers: COMPATIBILITY
+    }))
+    // Comment in the pipe below to run UnCSS in production
+    //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
     .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(PATHS.dist + '/assets/css'))
@@ -134,24 +93,19 @@ function vcardSass() {
 }
 
 let webpackConfig = {
-  mode: (PRODUCTION ? 'production' : 'development'),
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [ "@babel/preset-env" ],
-            compact: false
+        test: /.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
           }
-        }
+        ]
       }
     ]
-  },
-  devtool: !PRODUCTION && 'source-map'
+  }
 }
-
 // Combine JavaScript into one file
 // In production, the file is minified
 function javascript() {
@@ -159,7 +113,7 @@ function javascript() {
     .pipe(named())
     .pipe($.sourcemaps.init())
     .pipe(webpackStream(webpackConfig, webpack2))
-    .pipe($.if(PRODUCTION, $.terser()
+    .pipe($.if(PRODUCTION, $.uglify()
       .on('error', e => { console.log(e); })
     ))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
@@ -170,9 +124,9 @@ function javascript() {
 // In production, the images are compressed
 function images() {
   return gulp.src('src/assets/img/**/*')
-    .pipe($.if(PRODUCTION, $.imagemin([
-      $.imagemin.jpegtran({ progressive: true }),
-    ])))
+    .pipe($.if(PRODUCTION, $.imagemin({
+      progressive: true
+    })))
     .pipe(gulp.dest(PATHS.dist + '/assets/img'));
 }
 
@@ -180,7 +134,8 @@ function images() {
 function server(done) {
   browser.init({
     server: PATHS.dist, port: PORT
-  }, done);
+  });
+  done();
 }
 
 // Reload the browser with BrowserSync
@@ -194,10 +149,7 @@ function watch() {
   gulp.watch(PATHS.assets, copy);
   gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
-  gulp.watch('src/data/**/*.{js,json,yml}').on('all', gulp.series(resetPages, pages, browser.reload));
-  gulp.watch('src/helpers/**/*.js').on('all', gulp.series(resetPages, pages, browser.reload));
-  gulp.watch('src/assets/scss/**/*.scss').on('all', gulp.series(sass, vcardSass, browser.reload));
+  gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
   gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
   gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
-  gulp.watch('src/styleguide/**').on('all', gulp.series(styleGuide, browser.reload));
 }
